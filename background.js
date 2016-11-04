f60a2ef878811ecedc1aceea865fa04111d6126f
Guy Levy <guy@Guy.local>
2016-11-04 16:36:14 -0400

    removed background dependency and added MutationObserver to prevent text jumping

--- background.js
+++
@@ -1,7 +0,0 @@
-chrome.tabs.onUpdated.addListener(function(tabId, props) {
-    if (props.status !== 'complete') return;
-    chrome.tabs.sendMessage(tabId,
-        {
-            onUpdated: true
-        });
-});
--- content-script.js
+++ content-script.js
@@ -10,35 +10,40 @@
 */
 const patt = /[\u05D0-\u05EA]|[\u0620-\u063F]|[\u0641-\u064A]|[\u0675-\u06D3]|[\u0710-\u071C]|[\u071E-\u072F]|[\u074E-\u077F]|[\u08A0-\u08AC]|[\u08AE-\u08B4]|[\u07C1-\u07C9]|[\u07CC-\u07E9]/g
 
-let run_againts_article = (post_article) =>
+let fix_font_visual = (post_article) =>
 {
-    if (!patt.test(post_article.innerText)) return
-
-    post_article.style.direction = 'rtl'
-
-    // Fonts in Hebrew ususally look much beter on sans-serif
+    // Fonts in Hebrew usually look much better on sans-serif
     let paragraphs = post_article.querySelectorAll('p')
     let i = 0, len = paragraphs.length
-    for (; i < len; i++) paragraphs[i].style.fontFamily = 'sans-serif'
+    for (; i < len; i++)
+        paragraphs[i].style.fontFamily = 'sans-serif'
+}
 
+let run_against_article = (post_article) =>
+{
+    if (!patt.test(post_article.innerText))
+        return
+
+    post_article.style.direction = 'rtl'
+    fix_font_visual(post_article)
 }
 
 let run_on_page = () =>
 {
     let post_articles = document.querySelectorAll('.postArticle-content');
-    if (!post_articles.length) return;
+    if (!post_articles.length)
+        return
 
     let i = 0, len = post_articles.length
     for (; i < len; i++)
-        run_againts_article(post_articles[i])
-
+        run_against_article(post_articles[i])
 }
 
-chrome.runtime
-    .onMessage
-    .addListener(
-        (request) =>
+new MutationObserver(run_on_page)
+    .observe(
+        document.body,
         {
-            if (request.onUpdated)
-                run_on_page()
-        });
+            childList: true,
+            subtree: true
+        }
+    )
